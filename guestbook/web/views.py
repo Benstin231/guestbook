7be71c8512464df5a2bbaa6d953287c677912f86
Benstin231 <Benstin231@users.noreply.github.com>
2020-12-24 11:25:29 +0800

    新增web

---
+++ guestbook/web/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic import *
+from django.urls import reverse_lazy
+from .models import Message
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+
+
+class MessageList(ListView):
+    model = Message
+
+
+
+class MessageDetail(DetailView):
+    model = Message
+    
+
+
+
+class MessageCreate(CreateView):
+    model = Message
+    fields = '__all__'
+    success_url = reverse_lazy('msg_list')
+
+
+class MessageDelete(LoginRequiredMixin, DeleteView):
+    model = Message
+    success_url = reverse_lazy('msg_list')
